b40d4003c213800d44b1882aa52ef34d4b48709d
Mishagul <169081080+Mishagul@users.noreply.github.com>
2024-10-03 16:53:30 +0300

    fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,38 +1,53 @@
-export function handleSuccess(data) {
-  return data
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<li class="gallery-item">
-        <article class="card">
-    <a class="card-link" href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>        <div class="card-container">
-          <div class="card-item">
-            <p class="card-title">Likes</p>
-            <p class="card-count">${likes}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Views</p>
-            <p class="card-count">${views}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Comments</p>
-            <p class="card-count">${comments}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Downloads</p>
-            <p class="card-count">${downloads}</p>
-          </div>
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox;
+
+export function initializeLightbox() {
+  lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+export function refreshLightbox() {
+  if (lightbox) {
+    lightbox.refresh();
+  }
+}
+
+export function createImageCards(images, gallery) {
+  const markup = images.map(image => `
+    <li class="gallery-item">
+      <div class="photo-card">
+        <a href="${image.largeImageURL}" class="link">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item"><b class="name-info">Likes</b> ${image.likes}</p>
+          <p class="info-item"><b class="name-info">Views</b> ${image.views}</p>
+          <p class="info-item"><b class="name-info">Comments</b> ${image.comments}</p>
+          <p class="info-item"><b class="name-info">Downloads</b> ${image.downloads}</p>
         </div>
-        </article>
-          </li>`;
-      }
-    )
-    .join('');
+      </div>
+    </li>
+  `).join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  
+  refreshLightbox();
+}
+
+export function clearGallery(gallery) {
+  gallery.innerHTML = ''; 
+}
+
+export function showLoader(loadingIndicator) {
+  loadingIndicator.classList.remove('hidden');
+  loadingIndicator.style.display = 'block'; 
+}
+
+export function hideLoader(loadingIndicator) {
+  loadingIndicator.classList.add('hidden');
+  loadingIndicator.style.display = 'none'; 
 }
